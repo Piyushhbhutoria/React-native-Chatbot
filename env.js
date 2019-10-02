ec563a3fcb01fc37cc839ab943d7dc481310755b
Piyushhbhutoria <piyush.bhutoria98@gmail.com>
2019-10-02 15:00:51 +0530

    readme update

---
+++ env.js
@@ -0,0 +1,11 @@
+export const dialogflowConfig = {
+    type: "service_account",
+    project_id: "finatic",
+    private_key_id: "59fd29ca731fxxxxx",
+    private_key: "-----BEGIN PRIVATE KEY-----********=\n-----END PRIVATE KEY-----\n",
+    client_email: "***.iam.gserviceaccount.com",
+    client_id: "108659xxxxxx",
+    auth_uri: "https://accounts.google.com/xxxx",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/xxx",
+    client_x509_cert_url: "https://www.googleapis.com/robot/xxx/test-141%40finatic-f4060.iam.gserviceaccount.com"
