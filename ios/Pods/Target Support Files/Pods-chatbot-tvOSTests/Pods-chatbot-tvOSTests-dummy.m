20e5b2f996544ab4295da130eac0e33ee9b491d8
Piyushhbhutoria <piyush.bhutoria98@gmail.com>
2019-10-02 14:56:30 +0530

    rename, android and ios build fixed

---
+++ ios/Pods/Target Support Files/Pods-chatbot-tvOSTests/Pods-chatbot-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_chatbot_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_chatbot_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-chatbotTests/Pods-chatbotTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_chatbotTests : NSObject
+@end
+@implementation PodsDummy_Pods_chatbotTests
+@end
